2bd2c3f83cad464f16ae221cd8e85da62eabeadf
luciotato <luciotato@gmail.com>
2014-08-21 04:07:45 -0300

    now compiles with 0.8.7

--- source/browser/build-lite.sh
+++ source/browser/build-lite.sh
@@ -1,18 +1,2 @@
-#browser-hosted app
-
-OUTDIR="../../www/js"
-
-#use /devel/ compiler if it exists
-DEVCOMP=~/LiteScript/devel/generated-js/v0.8/lite-to-js/js_lite.js
-
-if [ -e "$DEVCOMP" ]; then 
-    #use /devel/ compiler
-    node $3 $DEVCOMP -browser index -v 2 -o $OUTDIR
-else
-    #else use npm installed litescript compiler
-    lite -browser index -v 2 -o $OUTDIR
-fi
-
-
-
-#lite -v 0 -compile jquery-tab-tab.lite.md -o ../../../product/www/js $*
+#compile browser-hosted app
+lite -browser index -v 2 -o ../../www/js
--- source/server/build-lite.sh
+++ source/server/build-lite.sh
@@ -1,15 +1,3 @@
 #server app
-
-OUTDIR="../../webServer"
-
-#use /devel/ compiler if it exists
-DEVCOMP=~/LiteScript/devel/generated-js/v0.8/lite-to-js/js_lite.js
-
-if [ -e "$DEVCOMP" ]; then 
-    #use /devel/ compiler
-    node $3 $DEVCOMP app -v 2 -o $OUTDIR
-else
-    #else use npm installed litescript compiler
-    lite app -o $OUTDIR
-fi
+lite app -o ../../webServer
 
